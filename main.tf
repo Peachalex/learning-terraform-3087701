578a206ea2a7a493134210940b186c1626efbcbf
Alex Tao <conquereralex@gmail.com>
2024-10-07 17:54:01 -0400

    modify http listener

--- main.tf
+++ main.tf
@@ -70,11 +70,12 @@ module "alb" {
     }
   }
 
-  http_tcp_listeners = {
-    port               = 80
-    protocol           = "HTTP"
-    target_group_index = 0
-  }
+  listeners = {
+    http-direct = {
+      port     = 80
+      protocol = "HTTP"
+      target_group_index = 0
+    }
 
   tags = {
     Environment = "Development"
