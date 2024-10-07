cc366099b005e13868c8ecc34cf4d5bd5fb648b9
Alex Tao <conquereralex@gmail.com>
2024-10-07 18:05:56 -0400

    array for id

--- main.tf
+++ main.tf
@@ -48,12 +48,12 @@ resource "aws_instance" "blog" {
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
-  load_balancer_type = "application"
+  
 
   name            = "blog-alb"
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  security_groups = [module.blog_sg.security_group_id]
 
   target_groups = {
     ex-instance = {
@@ -61,12 +61,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target ={
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+      target_id.       = aws_instance.blog.id
     }
   }
 
